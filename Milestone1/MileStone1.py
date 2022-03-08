ba9ecd4907cd80965f97156e950df3d9f52aa73c
Aishwarya-Jeyaraj <82449060+Aishwarya-Jeyaraj@users.noreply.github.com>
2022-03-08 10:59:40 +0530

    Added csv essentials

--- Milestone1/MileStone1.py
+++ Milestone1/MileStone1.py
@@ -1,5 +1,6 @@
 import yaml
 import datetime
+import csv 
 
 with open(r'E:\KLA\Milestone1\Milestone1A.yaml') as file:
     documents = yaml.full_load(file)
@@ -7,3 +8,7 @@ with open(r'E:\KLA\Milestone1\Milestone1A.yaml') as file:
     for item, doc in documents.items():
         print(datetime.datetime.now())
         print(item, ":", doc)
+        
+        with open('Milestone1.csv', 'w', encoding='UTF8', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(doc)
