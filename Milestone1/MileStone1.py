68c2dd0c819cba18d7e85d7e6c8178e8d2de1c9b
Aishwarya-Jeyaraj <82449060+Aishwarya-Jeyaraj@users.noreply.github.com>
2022-03-08 12:05:14 +0530

    Logging Display

--- Milestone1/MileStone1.py
+++ Milestone1/MileStone1.py
@@ -1,26 +1,49 @@
 import yaml
 import datetime
-import csv 
+import csv
 
 with open(r'E:\KLA\Milestone1\Milestone1A.yaml') as file:
     documents = yaml.full_load(file)
 
     for item, doc in documents.items():
-        
+
         now = datetime.datetime.now()
         print (now.strftime("%Y-%m-%d %H:%M:%S"),end="")
         print("000000")
-        
+
         print(item, ":", doc)
-        
+
         with open('Milestone1.csv', 'w', encoding='UTF8', newline='') as f:
             writer = csv.writer(f)
             writer.writerow(doc)
-            
+
+        rehearsal=0
+
         with open('Milestone1.txt', 'w') as f:
             for line in doc:
                 f.write(now.strftime("%Y-%m-%d %H:%M:%S"))
                 f.write("000000 ")
                 f.write(line)
-                f.write('\n')
+                
+        
+                print()
+
+                a_yaml_file = open("Milestone1A.yaml")
+                parsed_yaml_file = yaml.load(a_yaml_file, Loader=yaml.FullLoader)
+                print(parsed_yaml_file["M1A_Workflow"])
+
+                print()
+
+                try:
+                    for key,val in (parsed_yaml_file["M1A_Workflow"]):
+                        print(key,val)
+                except ValueError:
+                    print("M1A_Workflow Entry")
+                    if(rehearsal%2==0):
+                        f.write(" M1A_Workflow Entry")
+                    else:
+                        f.write(" M1A_Workflow Exit")
+                    f.write("\n")
+                    rehearsal+=1
+
 
