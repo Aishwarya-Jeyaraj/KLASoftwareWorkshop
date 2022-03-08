ef311feccd437a67ea8b335473ceafa68ede454c
Aishwarya-Jeyaraj <82449060+Aishwarya-Jeyaraj@users.noreply.github.com>
2022-03-08 11:13:37 +0530

    Date Time Format Modification

--- Milestone1/MileStone1.py
+++ Milestone1/MileStone1.py
@@ -6,7 +6,11 @@ with open(r'E:\KLA\Milestone1\Milestone1A.yaml') as file:
     documents = yaml.full_load(file)
 
     for item, doc in documents.items():
-        print(datetime.datetime.now())
+        
+        now = datetime.datetime.now()
+        print (now.strftime("%Y-%m-%d %H:%M:%S"),end="")
+        print("000000")
+        
         print(item, ":", doc)
         
         with open('Milestone1.csv', 'w', encoding='UTF8', newline='') as f:
