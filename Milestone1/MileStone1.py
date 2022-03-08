112d4bb31a9f14eda29def2aa953f9ced36e0d38
Aishwarya-Jeyaraj <82449060+Aishwarya-Jeyaraj@users.noreply.github.com>
2022-03-08 11:48:06 +0530

    .Txt file export

--- Milestone1/MileStone1.py
+++ Milestone1/MileStone1.py
@@ -16,3 +16,11 @@ with open(r'E:\KLA\Milestone1\Milestone1A.yaml') as file:
         with open('Milestone1.csv', 'w', encoding='UTF8', newline='') as f:
             writer = csv.writer(f)
             writer.writerow(doc)
+            
+        with open('Milestone1.txt', 'w') as f:
+            for line in doc:
+                f.write(now.strftime("%Y-%m-%d %H:%M:%S"))
+                f.write("000000 ")
+                f.write(line)
+                f.write('\n')
+
